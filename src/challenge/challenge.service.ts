e5468dfd05f98be8c395a6224e1df185b1649e8a
minjae1025 <kjt081025@gmail.com>
2026-05-20 19:25:31 +0900

    챌린지 검색 및 조회

--- src/challenge/challenge.service.ts
+++ src/challenge/challenge.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Challenge } from './entities/challenge.entity';
+import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { NotFoundException } from '@nestjs/common';
+import { CreateMusicDto } from './dto/create-music.dto';
 
 @Injectable()
-export class ChallengeService {}
+export class ChallengeService {
+    constructor(@Inject('ChallengeRepository') private readonly challengeRepository: Repository<Challenge>, private readonly configService: ConfigService) { }
+
+    async getChallenges(id: number) {
+        const result = await this.challengeRepository.findOne({ where: { id } });
+
+        if (!result) {
+            throw new NotFoundException('챌린지를 찾을 수 없습니다.');
+        }
+
+        return result;
+    }
+
+    async createChallenge(createMusicDto: CreateMusicDto) {
+        const challenge = this.challengeRepository.create(createMusicDto);
+        return await this.challengeRepository.save(challenge);
+    }
+
+    async searchChallenges(keyword: string) {
+        const result = await this.challengeRepository
+            .createQueryBuilder('challenge')
+            .where('challenge.name ILIKE :keyword', { keyword: `%${keyword}%` })
+            .orWhere('challenge.artist ILIKE :keyword', { keyword: `%${keyword}%` })
+            .getMany(); 
+
+        return result;
+    }
+}
